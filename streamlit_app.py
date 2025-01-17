98600188bf2bc6087560902711a4d1889f635a73
YoussefCreationX <thesisnest2022@gmail.com>
2025-01-17 11:35:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
-
+import pandas as pd 
 st.title('Stress Detection APP')
 st.info('This is a stress detection app based on physiological data and machine learning model')
 
-st.write('Hello world!')
+with st.expander('Physiological dataset used from a smartwatch'):
+  st.write('**Raw data**')
+  df = pd.read_csv('LeftDataSetA2Fin.csv')
+  df
