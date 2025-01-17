6ae75485fdc0568a624fc2201607539b7b111729
YoussefCreationX <thesisnest2022@gmail.com>
2025-01-17 11:39:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,11 @@ with st.expander('Physiological dataset used from a smartwatch'):
   st.write('**Raw data**')
   df = pd.read_csv('LeftDataSetA2Fin.csv')
   df
+
+st.write('**X**')
+X = df.drop('danger_level',axis=1)
+X 
+
+st.write('**y**')
+y = df.danger_level
+y
