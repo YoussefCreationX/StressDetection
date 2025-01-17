0aa97af712c29482d598e1c6627622f1b6f07a03
YoussefCreationX <thesisnest2022@gmail.com>
2025-01-17 12:20:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,17 @@ with st.expander('Physiological dataset used from a smartwatch'):
   st.write('**y**')
   y = df.danger_level
   y
-with st.expander('Data visualizationj'):
+with st.expander('Data visualization'):
    st.scatter_chart(data=df, x='EDA',y='TEMP',color='danger_level')
 # Data Preparations 
+# ACC_x,ACC_y,ACC_z,BVP,EDA,TEMP,HR,IBI_intervals,danger_level
 with st.sidebar: 
   st.header('Input features')
-  acc_x = st.slider('acc_x (m/s²)',-35,30)
+  acc_x = st.slider('acc_x (m/s²)',-60,60)
+  acc_y = st.slider('acc_y (m/s²)',-60,60)
+  acc_z = st.slider('acc_z (m/s²)',-60,60)
+  BVP = st.slider('BVP ()',0,10)
+  EDA = st.slider('EDA (mS)',0,1)
+  TEMP = st.slider('TEMP (c°)',15,40)
+  HR = st.slider('HR ()',70,120)
+  IBI_intervals = st.slider('IBI_intervals ()',0,1)
