26518f9fd620bae4785955a7e85ff33145936055
YoussefCreationX <thesisnest2022@gmail.com>
2025-01-17 13:11:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ def predict(data:dict):
 # Display predicted species 
 st.subheader('Predicted Species')
 prediction = predict(data)
-st.success(prediction)
+st.success(prediction.values())
 prediction
    
   
